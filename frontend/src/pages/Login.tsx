1de04e64f128f39b440fe2162a5c21e3a983abf8
Kaustuv Karki <karkikaustuv@gmail.com>
2024-06-15 16:56:13 +0545

    working on shadcn error

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import LoginComponent from "@/components/LoginComponent/LoginComponent";
+
+const Login = () => {
+  return (
+    <div>
+      <p className="text-[3rem]">This</p>
+      <LoginComponent />
+    </div>
+  );
+};
+
+export default Login;

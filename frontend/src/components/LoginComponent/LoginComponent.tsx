f6a03643e57eabab57a2d65e8f18825fb0c2b8eb
Kaustuv Karki <karkikaustuv@gmail.com>
2024-06-15 18:07:28 +0545

    Login page UI done

---
+++ frontend/src/components/LoginComponent/LoginComponent.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+const LoginComponent = () => {
+  const form = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-start">
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Username" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-start">
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" variant="secondary" className="w-full">
+            Log In
+          </Button>
+        </form>
+        <Button variant="link" className="w-full text-white">
+          Create New Account
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginComponent;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,21 @@
-import LoginComponent from "@/components/ui/LoginComponent/LoginComponent";
+import LoginComponent from "@/components/LoginComponent/LoginComponent";
 
 const Login = () => {
   return (
-    <div>
-      <p className="text-[3rem] text-red-500">This</p>
-      <LoginComponent />
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col items-center w-full">
+        <div className="md:mb-[5rem] mb-[3rem] flex flex-col justify-center items-center">
+          <h1 className="text-white font-semibold md:text-[3rem] text-[2rem]">
+            Login to Your Account
+          </h1>
+          <p className="text-gray-300 md:text-[1.2rem] text-[1rem]">
+            Handle All the artist information
+          </p>
+        </div>
+        <div className="max-w-[600px] w-full border md:px-8 md:py-10 px-4 py-5">
+          <LoginComponent />
+        </div>
+      </div>
     </div>
   );
 };

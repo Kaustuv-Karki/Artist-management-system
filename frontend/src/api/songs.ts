eff9d6dee6c21960919bae3f18c7d0a66a6094f3
Kaustuv Karki <karkikaustuv@gmail.com>
2024-06-16 22:24:36 +0545

    Songs issue solved

--- frontend/src/api/songs.ts
+++ frontend/src/api/songs.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
-export const fetchSongsById = async (id: string) => {
+
+export const fetchSongsById = async (artistId: string) => {
+    console.log("Fetch songs called", artistId)
     try {
-        const response = await axios.get(`http://localhost:5000/api/music/artist/${id}`);
+        const response = await axios.get(`http://localhost:5000/api/music/artist/${artistId}`);
         return response.data;
     } catch (error) {
         console.error(error);
--- frontend/src/pages/SongsList.tsx
+++ frontend/src/pages/SongsList.tsx
@@ -1,16 +1,117 @@
 import { fetchSongsById } from "@/api/songs";
+import { TableComponent } from "@/components/TableComponent/TableComponent";
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
 const SongsList = () => {
+  console.log("Songs list called");
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
   const { artistId } = useParams();
-  const { data } = useQuery({
+  console.log("Artist id", artistId);
+  const { data, isLoading } = useQuery({
     queryKey: ["songs", artistId],
     queryFn: () => fetchSongsById(artistId),
-    enabled: !!artistId,
   });
-  console.log(data);
-  return <div>SongsList</div>;
+
+  const songsColumns = [
+    {
+      accessorKey: "id",
+      header: "ID",
+      cell: ({ row }) => <div>{row.getValue("id")}</div>,
+    },
+    {
+      accessorKey: "title",
+      header: "Title",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("title")}</div>
+      ),
+    },
+    {
+      accessorKey: "album_name",
+      header: "Album Name",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("album_name")}</div>
+      ),
+    },
+    {
+      accessorKey: "email",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() =>
+              column.toggleSorting(column.getIsSorted() === "asc")
+            }>
+            Email
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="lowercase">{row.getValue("email")}</div>
+      ),
+    },
+    {
+      accessorKey: "phone",
+      header: "Phone",
+      cell: ({ row }) => <div>{row.getValue("phone")}</div>,
+    },
+    {
+      accessorKey: "dob",
+      header: "Date of Birth",
+      cell: ({ row }) => <div>{row.getValue("dob")}</div>,
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      cell: ({ row }) => {
+        const user = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigate(`/edit-user/${user.id}`)}>
+                Edit
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Delete</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+  console.log("Song data", !isLoading && data.data);
+  return (
+    <div>
+      <TableComponent
+        columns={songsColumns}
+        data={data.data}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+    </div>
+  );
 };
 
 export default SongsList;
